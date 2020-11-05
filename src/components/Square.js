d1c630f1ddefcd0ac36a8fc8d47c1c5f8c11dc1e
zhezhixin <zhezhixin@sina.cn>
2020-11-05 17:06:43 +0800

    add 井字棋

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,19 +1,36 @@
-import React, { Component } from "react"
+import React from "react"
 
-class Square extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            value:null,
-        };
-    }
-    render() {
-        return (
-        <button className="square" onClick={()=>this.setState({value:'X'})}>
-            {this.state.value}
+function Square(props){
+    return (
+        <button 
+            className="square" 
+            onClick={props.onMyClick}
+        >
+            {props.value}
         </button>
-      );
-    }
+    );
 }
+
+//以下是类继承于React.Component,如果只有render方法，不包含state,使用函数组件
+//import {Component} from "react"
+// class Square extends Component {
+//     // constructor(props){
+//     //     super(props)
+//     //     this.state = {
+//     //         value:null,
+//     //     };
+//     // }
+//     render() {
+//         return (
+//         <button 
+//             className="square" 
+//             onClick={()=>this.props.onMyClick()}
+//         >
+//            {this.props.value}
+//         </button>
+//       );
+//     }
+// }
+
   
 export default Square
