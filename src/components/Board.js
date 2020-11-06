eb629a71cc88dca0b9753a1d83d4ebeb5f2d0486
zhezhixin <zhezhixin@sina.cn>
2020-11-06 12:46:11 +0800

    add 历史记录 state 从Board 提到Game

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,46 +2,46 @@ import React, { Component } from "react"
 import Square from './Square'
 
 class Board extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            squares:Array(9).fill(null),
-            xIsNext:true
-        };
-    }
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+    //         squares:Array(9).fill(null),
+    //         xIsNext:true
+    //     };
+    // }
 
-    handleClick(i) {
-        const squares = this.state.squares.slice(); //跟踪不可变数据的变化相对来说就容易多了。如果发现对象变成了一个新对象，那么我们就可以说对象发生改变了。
-        if (calculateWinner(squares) || squares[i]){
-            return;
-        }
-        squares[i] = this.state.xIsNext?'X':'O';
-        this.setState({
-            squares:squares,
-            xIsNext:!this.state.xIsNext,
-        })
-    }
+    // handleClick(i) {
+    //     const squares = this.state.squares.slice(); //跟踪不可变数据的变化相对来说就容易多了。如果发现对象变成了一个新对象，那么我们就可以说对象发生改变了。
+    //     if (calculateWinner(squares) || squares[i]){
+    //         return;
+    //     }
+    //     squares[i] = this.state.xIsNext?'X':'O';
+    //     this.setState({
+    //         squares:squares,
+    //         xIsNext:!this.state.xIsNext,
+    //     })
+    // }
 
     renderSquare(i) {
         return (
             <Square 
-                value={this.state.squares[i]}
-                onMyClick={()=>this.handleClick(i)}
+                value={this.props.squares[i]}
+                onMyClick={()=>this.props.onGameClick(i)}
             />
         );
     }
 
     render() {
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if (winner){
-            status = "winner: " + winner;
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext?'X':'O');
-        }
+        // const winner = calculateWinner(this.state.squares);
+        // let status;
+        // if (winner){
+        //     status = "winner: " + winner;
+        // } else {
+        //     status = 'Next player: ' + (this.state.xIsNext?'X':'O');
+        // }
         return (
         <div>
-            <div className="status">{status}</div>
+            {/* <div className="status">{status}</div> */}
             <div className="board-row">
                 {this.renderSquare(0)}
                 {this.renderSquare(1)}
@@ -61,23 +61,23 @@ class Board extends Component {
         );
     }
 }
-function calculateWinner(squares){
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++){
-        const [a,b,c] = lines[i]
-        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-            return squares[a]
-        }
-    }
-    return null
-}
+// function calculateWinner(squares){
+//     const lines = [
+//         [0, 1, 2],
+//         [3, 4, 5],
+//         [6, 7, 8],
+//         [0, 3, 6],
+//         [1, 4, 7],
+//         [2, 5, 8],
+//         [0, 4, 8],
+//         [2, 4, 6],
+//     ];
+//     for (let i = 0; i < lines.length; i++){
+//         const [a,b,c] = lines[i]
+//         if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+//             return squares[a]
+//         }
+//     }
+//     return null
+// }
 export default Board
